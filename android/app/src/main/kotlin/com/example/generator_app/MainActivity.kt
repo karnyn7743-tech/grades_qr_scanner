8eb504014fdd070ed7dc2109003dd25ced72ba96
أبو الخضر البعيثي <karnyn7743@gmail.com>
2026-08-13 12:46:04 +0300

    Refactor MainActivity class structure

--- android/app/src/main/kotlin/com/example/generator_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/generator_app/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.generator_app
+package com.example.generator_app // أو اسم الباكيج الخاص بمشروعك القديم
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
